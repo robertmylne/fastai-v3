ec11245c61abf9773110e997705252d9e3535178
Robert Mylne <robertmylne@live.com>
2020-03-09 15:45:12 +1000

    -

--- app/server.py
+++ app/server.py
@@ -73,4 +73,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info", workers=2)
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")

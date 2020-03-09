4607d55da133f115b8f8593f172a6e046c23fb69
Robert Mylne <robertmylne@live.com>
2020-03-09 14:46:21 +1000

    -

--- pull.sh
+++ pull.sh
@@ -1,3 +1,4 @@
 git pull
-docker build -t fastai-v3 . 
-docker run --rm -d -p 5000:5000 fastai-v3
+docker build -t fastai-v3 .
+docker container stop dog_breeds_ai
+docker run --rm -d -p 5000:5000 --name dog_breeds_ai  fastai-v3
